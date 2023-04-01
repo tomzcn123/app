1be2d7f40b6c12674cab0d193d0e7098d4488ed7
tomzcn123 <79131150+tomzcn123@users.noreply.github.com>
2023-03-31 21:38:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,27 @@
-import pandas as pd
-import pandas_datareader.data as web
 import streamlit as st
-import datetime as dt
-import plotly.graph_objs as go
+import yfinance as yf
+import pandas as pd
+import plotly.express as px
 
+# Set up the app title and description
 st.title("Stock Visualization App")
+st.write("""
+Simple app to visualize stock price data. Enter a stock symbol and select a date range.
+""")
 
-# Input stock ticker and date range
-ticker = st.text_input("Enter a stock ticker:", "AAPL")
-start_date = st.date_input("Start date:", dt.datetime(2020, 1, 1))
-end_date = st.date_input("End date:", dt.datetime.now())
+# Get the user input for stock symbol and date range
+stock_symbol = st.text_input("Enter Stock Symbol", "AAPL")
+start_date = st.date_input("Start Date", value=pd.to_datetime("2020-01-01"))
+end_date = st.date_input("End Date", value=pd.to_datetime("today"))
 
-# Fetch stock data using pandas-datareader
-try:
-    data = web.get_data_yahoo(ticker, start=start_date, end=end_date)
-except Exception as e:
-    st.write(f"Error fetching data: {e}")
-    data = pd.DataFrame()
+# Download stock data using yfinance
+stock_data = yf.download(stock_symbol, start=start_date, end=end_date)
 
-# Check if data is available
-if data.empty:
-    st.write("No data available for the selected ticker and date range.")
-else:
-    # Display the fetched data
-    st.write("Stock data:")
-    st.dataframe(data)
+# Display the stock data as a table
+st.write("Stock Data")
+st.write(stock_data)
 
-    # Plot the stock data
-    fig = go.Figure(
-        go.Candlestick(
-            x=data.index,
-            open=data["Open"],
-            high=data["High"],
-            low=data["Low"],
-            close=data["Close"],
-            name=ticker,
-        )
-    )
-
-    fig.update_layout(title=f"{ticker} Stock Price", xaxis_title="Date", yaxis_title="Price (USD)")
-
-    st.write("Stock price chart:")
-    st.plotly_chart(fig)
-
+# Plot the stock data with Plotly
+fig = px.line(stock_data, x=stock_data.index, y='Close', title=f'{stock_symbol} Closing Price')
+st.write("Stock Price Chart")
+st.plotly_chart(fig)
