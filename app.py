f9f4a56f1ff212e78580493e344992e71f57a1f4
tomzcn123 <79131150+tomzcn123@users.noreply.github.com>
2023-04-18 15:20:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,14 +18,16 @@ end_date = st.sidebar.date_input("End Date", value=pd.to_datetime("today"))
 # Download stock data using yfinance
 stock_data = yf.download(stock_symbol, start=start_date, end=end_date)
 
-# Display the stock data as a table
-st.write("Stock Data")
-st.write(stock_data)
-
 # Plot the stock data with Plotly
 fig = px.line(stock_data, x=stock_data.index, y='Close', title=f'{stock_symbol} Closing Price')
 st.write("Stock Price Chart")
 st.plotly_chart(fig)
+
+# Display the stock data as a table
+st.write("Stock Data")
+st.write(stock_data)
+
+
 
 
 
